7cc1f7bdddf025b6c4d268e51b5d9e5dbaf9d06a
Markiian Mandzak <markmandzak2002@gmail.com>
2025-02-13 11:48:23 +0200

    First Commit

---
+++ postprocess.py
@@ -0,0 +1,113 @@
+# Postprocessing
+import os
+import argparse
+import json
+import numpy as np
+import scipy.ndimage
+import nibabel as nib
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Postprocessing')
+    parser.add_argument('--mr_input',  '-mi', type=str, required=True,  help='MRI input directory')
+    parser.add_argument('--preds_input',  '-pi', type=str, required=True,  help='Predicitons input directory')
+    parser.add_argument('--mr_output', '-mo', type=str, required=True, help='MRI output directory')
+    parser.add_argument('--preds_output', '-po', type=str, required=True, help='Predicitons output directory')
+    parser.add_argument('--deface', '-d', action='store_true', help='Deface the MRI and predictions')
+    args = parser.parse_args()
+    return args
+
+def keep_largest_component(seg: np.ndarray) -> np.ndarray:
+    assert np.array_equal(np.unique(seg), [0, 1]), 'Segmentation mask must be binary (0 and 1)'
+
+    labeled_array, num_features = scipy.ndimage.label(seg)  # Label connected components
+    if num_features == 0:
+        return seg  # Return the same mask if no components exist
+
+    component_sizes = scipy.ndimage.sum(seg, labeled_array, range(1, num_features + 1))
+    largest_label = np.argmax(component_sizes) + 1
+
+    cleaned_seg = (labeled_array == largest_label).astype(np.uint8)
+    return cleaned_seg
+
+def deface_nii_left(mri: np.array, brain_seg: np.array, background_value: int):
+    brain_seg_mask = brain_seg > 0
+    first_col_idxs_with_nonzero_value = []
+    for z in range(mri.shape[2]):
+        slice_mask = brain_seg_mask[:, :, z]
+        first_column_with_nonzero_value = np.argmax(np.sum(np.abs(slice_mask), axis=0) > 0)
+        first_col_idxs_with_nonzero_value.append(first_column_with_nonzero_value)
+    
+    lobe_slice_idx = np.argmin([i if i > 0 else np.inf for i in first_col_idxs_with_nonzero_value])
+    for z in range(0, lobe_slice_idx):
+        first_col_idxs_with_nonzero_value[z] = first_col_idxs_with_nonzero_value[lobe_slice_idx]
+    
+    mri_defaced = mri.copy()
+    for z, col_idx in enumerate(first_col_idxs_with_nonzero_value):
+        if col_idx == 0 and z > lobe_slice_idx:
+            col_idx = mri.shape[1]
+        mri_defaced[:, :col_idx, z] = background_value
+    return mri_defaced
+
+def deface_mri_left(mri: np.array, brain_seg: np.array):
+    return deface_nii_left(mri, brain_seg, background_value=0)
+
+def main(args):
+    assert os.path.exists(args.mr_input), f'MRI input directory not found: {args.mr_input}'
+    assert os.path.exists(args.preds_input), f'Predictions input directory not found: {args.preds_input}'
+    ds_json_path = os.path.join(args.preds_input, 'dataset.json')
+    assert os.path.exists(ds_json_path), f'dataset.json file not found in: {args.preds_input}'
+    ds_json = json.load(open(ds_json_path))
+    brain_val = ds_json['labels']['brain']
+    print(f'Creating output directory {args.mr_output} ...')
+    os.makedirs(args.mr_output, exist_ok=True)
+    print(f'Creating output directory {args.preds_output} ...')
+    os.makedirs(args.preds_output, exist_ok=True)
+    pred_files = sorted(os.listdir(args.preds_input))
+    print("Scanning input directories ...")
+    paths_pairs = []
+    for file in sorted(pred_files):
+        if not file.endswith('.nii.gz'):
+            print(f"Skipping {file} (not a nii.gz file)")
+            continue
+        sample_name = file.split('.')[0]
+        mr_path = os.path.join(args.mr_input, f'{sample_name}_0000.nii.gz')
+        pred_path = os.path.join(args.preds_input, file)
+        assert os.path.exists(mr_path), f'MR file not found: {mr_path}'
+        assert os.path.exists(pred_path), f'Predictions file not found: {pred_path}'
+        paths_pairs.append((mr_path, pred_path))
+    print(f"Found {len(paths_pairs)} pairs of files. Processing ...")
+    for i, (mr_path, pred_path) in enumerate(paths_pairs):
+        print(f"[{i+1}/{len(paths_pairs)}] Processing {mr_path} and {pred_path} ...")
+        mr = nib.load(mr_path)
+        mr_data = mr.get_fdata()
+        pred = nib.load(pred_path)
+        pred_data = pred.get_fdata()
+        
+        # Copy data
+        mr_out_data   = mr_data.copy()
+        pred_out_data = pred_data.copy()
+        # Filter brain mask
+        brain_mask = (pred_data == brain_val).astype(np.uint8)
+        brain_mask_filtered = keep_largest_component(brain_mask)
+        pred_out_data[(pred_data == brain_val) & (brain_mask_filtered == 0)] = 0
+        
+        if args.deface:
+            # Deface MRI
+            mr_out_data = deface_mri_left(mr_out_data, brain_mask_filtered)
+            pred_out_data = deface_nii_left(pred_out_data, brain_mask_filtered, background_value=0)
+        
+        # Create output paths
+        mr_out_path   = os.path.join(args.mr_output, os.path.basename(mr_path))
+        pred_out_path = os.path.join(args.preds_output, os.path.basename(pred_path))
+        # Create output files
+        mr_out_file   = nib.Nifti1Image(mr_out_data, mr.affine) 
+        pred_out_file = nib.Nifti1Image(pred_out_data, pred.affine)
+        # Save output files
+        nib.save(mr_out_file, mr_out_path)
+        nib.save(pred_out_file, pred_out_path)
+        
+    print("Done!")
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
---
+++ preprocess.py
@@ -0,0 +1,86 @@
+# Preprocessing
+import os
+import argparse
+import nibabel as nib
+import SimpleITK as sitk
+from nibabel.orientations import aff2axcodes, axcodes2ornt, ornt_transform
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Preprocessing')
+    parser.add_argument('--input',  '-i', type=str, required=True,  help='Input directory')
+    parser.add_argument('--output', '-o', type=str, required=False, help='Output directory (optional)')
+    args = parser.parse_args()
+    if not args.output:
+        args.output = args.input
+    return args
+
+def read_nii_with_fix(file_path):
+    """
+    Attempts to load a NIfTI file using nibabel. If loading fails, tries to fix it using SimpleITK.
+
+    :param file_path: Path to the NIfTI file.
+    :type file_path: str
+    :return: Loaded NIfTI file if successful, otherwise None.
+    :rtype: nib.Nifti1Image or None
+    :raises Exception: If the file cannot be loaded or fixed.
+    """
+    file = None
+    try:
+        file = nib.load(file_path)
+    except Exception as e:
+        print(f'\033[91mError loading {file_path}: {e}\033[0m')
+        print("Trying to fix the currupted file...")
+        try:
+            sitk_img = sitk.ReadImage(file_path)
+            sitk.WriteImage(sitk_img, file_path)
+            file = nib.load(file_path)
+            print("Fixed the corrupted file succesfully!")
+        except Exception as e:
+            print(f'\033[91mError loading {file_path}: {e}\033[0m')
+            print()
+    return file
+
+def reorient_to_lpi(file: nib.Nifti1Image):
+    """
+    Reorients a NIfTI file to LPI (Left-Posterior-Inferior) orientation.
+
+    :param file: The NIfTI image to be reoriented.
+    :type file: nib.Nifti1Image
+    :return: The reoriented NIfTI image.
+    :rtype: nib.Nifti1Image
+    """
+    original_ornt = axcodes2ornt(aff2axcodes(file.affine))
+    target_ornt = axcodes2ornt(('L', 'P', 'I'))
+    transform = ornt_transform(original_ornt, target_ornt)
+    reoriented_file = file.as_reoriented(transform)
+    return reoriented_file
+
+def main(args):
+    os.makedirs(args.output, exist_ok=True)
+    files = os.listdir(args.input)
+    for i, file_name in enumerate(sorted(os.listdir(args.input))):
+        file_path = os.path.join(args.input, file_name)
+        if not file_path.endswith('.nii.gz'):
+            print(f"[{i+1}/{len(files)}] Skipping {file_name} (not a nii.gz file)")
+            continue
+        print(f"[{i+1}/{len(files)}] Processing {file_path}...")
+        file = read_nii_with_fix(file_path)
+        if aff2axcodes(file.affine) != ('L', 'P', 'I'):
+            print(f"\tFound orientation {aff2axcodes(file.affine)}. Reorienting to LPI...")
+            file = reorient_to_lpi(file)
+        else:
+            print("\tAlready LPI")
+        
+        output_file_name = None
+        if file_name.endswith('_0000.nii.gz'):
+            output_file_name = file_name
+        else:
+            output_file_name = file_name.replace('.nii.gz', '_0000.nii.gz')
+        output_file_path = os.path.join(args.output, output_file_name)
+        nib.save(file, output_file_path)
+        print(f"\tSaved to {output_file_path}")
+        print()
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
